e24d83173081de0fec78686b13962ab996364d81
Dmitrii Iakovlev <iakovlev.dmitrii@gmail.com>
2021-07-20 22:04:56 +0300

    Добавил страницу-заглушку со списком статей

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
+import ArticleList from "../article-list";
+import styles from './styles/app.module.scss';
+
+const { app } = styles;
 
 const App = () => (
-   <div>
-      <p>Hello World</p>
+   <div className={app}>
+      <ArticleList />
    </div>
 );
 
