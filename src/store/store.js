37d3e5ea5b1ac4b1585781c147864c14fb46067b
Dmitrii Iakovlev <iakovlev.dmitrii@gmail.com>
2022-01-08 00:00:10 +0300

    store

--- src/store/store.js
+++ src/store/store.js
@@ -3,25 +3,6 @@ import thunkMiddleware from 'redux-thunk';
 import reducer from './reducers';
 import { loadFromLocalStorage, saveToLocalStorage } from '../utils';
 
-// const saveToLocalStorage = (state) => {
-//   try {
-//     const serialisedState = JSON.stringify(state);
-//     localStorage.setItem('realworldStr', serialisedState);
-//   } catch (error) {
-//     console.warn(error);
-//   }
-// };
-// const loadFromLocalStorage = () => {
-//   try {
-//     const serialisedState = localStorage.getItem('realworldStr');
-//     if (serialisedState === null) return undefined;
-//     return JSON.parse(serialisedState);
-//   } catch (error) {
-//     console.warn(error);
-//     return undefined;
-//   }
-// };
-
 const composeEnhancers =
   typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__
     ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({})
