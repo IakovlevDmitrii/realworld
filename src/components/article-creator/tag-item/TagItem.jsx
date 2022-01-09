2856fa60f9eff7bf9b1471ab432dc1901e5b4787
Dmitrii Iakovlev <iakovlev.dmitrii@gmail.com>
2022-01-09 22:05:27 +0300

    create TagItem component

--- src/components/article-creator/tag-item/TagItem.jsx
+++ src/components/article-creator/tag-item/TagItem.jsx
@@ -3,15 +3,17 @@ import PropTypes from 'prop-types';
 import styles from "./TagItem.module.scss";
 
 const TagItem = ({ index, register }) => (
-   <div className={styles.tagContainer}>
+   <div className={styles.container}>
       <input
          type='text'
          placeholder='Tag'
-         className={styles.tagInput}
+         className={styles.input}
          {...register(`tagList.${index}`)} />
       <button
-         className={styles.deleteButton}
-         type='button'>Delete</button>
+         className={styles.button}
+         type='button'>
+         Delete
+      </button>
    </div>
 );
 
