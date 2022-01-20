4d7e1fe11695ef910acc8e17a6ed335a52569948
Dmitrii Iakovlev <iakovlev.dmitrii@gmail.com>
2022-01-20 19:36:29 +0300

    FormField with labelBehind

--- src/components/authComponents/components/FormField.jsx
+++ src/components/authComponents/components/FormField.jsx
@@ -2,22 +2,49 @@ import React from 'react';
 import PropTypes from "prop-types";
 import styles from "../styles/authComponents.module.scss";
 
-const FormField = ({ label, name, placeholder, type, register, validationRules, errors }) => {
+const FormField = ( {
+   label,
+   name,
+   placeholder,
+   type,
+   register,
+   validationRules,
+   errors,
+   labelBehind,
+} ) => {
    const getSubInput = (inputName) => (
       errors[inputName] && (
          <span>{errors[inputName].message}</span>
       )
    );
 
+   const elements = {
+      label: <label htmlFor={name}>{label}</label>,
+      input: <input
+         className={errors[name] && styles.error}
+         placeholder={placeholder}
+         type={type}
+         {...register(name, {...validationRules})}
+      />,
+   };
+
+   const elementsToShow = () => (
+      labelBehind ? (
+         <>
+            {elements.input}
+            {elements.label}
+         </>
+         ) : (
+         <>
+            {elements.label}
+            {elements.input}
+         </>
+      )
+   );
+
    return (
       <div className={styles.field}>
-         <label htmlFor={name}>{label}</label>
-         <input
-            className={errors[name] && styles.error}
-            placeholder={placeholder}
-            type={type}
-            {...register(name, {...validationRules})}
-         />
+         {elementsToShow()}
          {getSubInput(name)}
       </div>
    )
@@ -31,6 +58,11 @@ FormField.propTypes = {
    register: PropTypes.func.isRequired,
    validationRules: PropTypes.shape({ }).isRequired,
    errors: PropTypes.shape({ }).isRequired,
+   labelBehind: PropTypes.bool,
+};
+
+FormField.defaultProps = {
+   labelBehind: false,
 };
 
 export default FormField;
