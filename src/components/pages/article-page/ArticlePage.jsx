025bc768ce6f880c17132527e9eaa54ef27abf0f
Dmitrii Iakovlev <iakovlev.dmitrii@gmail.com>
2022-01-30 00:13:44 +0300

    ArticlePage without store

--- src/components/pages/article-page/ArticlePage.jsx
+++ src/components/pages/article-page/ArticlePage.jsx
@@ -1,34 +1,28 @@
 import React, { useState, useEffect, useCallback } from 'react';
-import { connect } from "react-redux";
-import PropTypes from 'prop-types';
+import { useParams } from 'react-router-dom';
 
 import realWorldApiService from '../../../service';
-import articleCreators from '../../../store/action-creators';
 
-import Article from '../../articles/article';
+import Article from '../../article';
 import Spinner from "../../spinner";
 import ErrorIndicator from "../../errors/error-indicator";
 
 import styles from './ArticlePage.module.scss';
 
-const ArticlePage = ({ articleData, setArticle, clearArticleData }) => {
-   const { article, isTheArticleNew } = articleData;
-   const { slug } = article;
+const ArticlePage = () => {
+   const { slug } = useParams();
 
+   const [ article, setArticle ] = useState({});
    const [ hasError, setHasError ] = useState(false);
    const [ isLoading, setIsLoading ] = useState(true);
 
-   // Если в store появится slug
    const loadArticle = useCallback(() => {
-
-      if (!isTheArticleNew) {
-         setIsLoading(true);
 
          realWorldApiService
             .articles
             .getArticle(slug)
-            .then((data) => {
-               setArticle(data);
+            .then((content) => {
+               setArticle(content);
             })
             .catch(() => {
                setHasError(true);
@@ -36,22 +30,14 @@ const ArticlePage = ({ articleData, setArticle, clearArticleData }) => {
             .finally(() => {
                setIsLoading(false);
             })
-      }
-
-         setIsLoading(false);
       },
-      [slug, isTheArticleNew, setArticle]
+      [slug, setArticle]
    );
 
    useEffect(
       () => loadArticle(),
       [loadArticle]
    );
-
-   // после размонтирования удалим информацию о статье из store
-   useEffect(() => (
-      () => {clearArticleData()}
-   ), [clearArticleData]);
 
    if(isLoading) {
       return <Spinner />
@@ -70,52 +56,4 @@ const ArticlePage = ({ articleData, setArticle, clearArticleData }) => {
    )
 };
 
-ArticlePage.propTypes = {
-   articleData: PropTypes.shape({
-      article: PropTypes.shape({
-         author: PropTypes.shape({
-            image: PropTypes.string,
-            username: PropTypes.string
-         }),
-         createdAt: PropTypes.string,
-         favorited: PropTypes.bool,
-         favoritesCount: PropTypes.number,
-         slug: PropTypes.string,
-         tagList: PropTypes.arrayOf(PropTypes.string),
-         title: PropTypes.string
-      }),
-      isTheArticleNew: PropTypes.bool.isRequired
-   }),
-   clearArticleData: PropTypes.func.isRequired,
-   setArticle: PropTypes.func.isRequired,
-};
-
-ArticlePage.defaultProps = {
-   articleData: {
-      article: {
-         author: {
-            image: '',
-            username: ''
-         },
-         body: '',
-         createdAt: '',
-         favorited: false,
-         favoritesCount: 0,
-         slug: '',
-         tagList: [],
-         title: ''
-      }
-   }
-};
-
-const mapStateToProps = ({ articleData }) => ({ articleData });
-
-const mapDispatchToProps = {
-   setArticle: articleCreators.articleData.setArticle,
-   clearArticleData: articleCreators.articleData.clearArticleData,
-};
-
-export default connect(
-   mapStateToProps,
-   mapDispatchToProps
-)(ArticlePage);
+export default ArticlePage;
